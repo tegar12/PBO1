d2460a2855323c8375ab041d94d8816888ea3949
tegar iman <44028234+tegar12@users.noreply.github.com>
2018-10-13 20:54:01 +0700

    Create Shinobi.java

---
+++ Shinobi.java
@@ -0,0 +1,21 @@
+public class Shinobi {
+  private String noregistrasi, nama, peringkat;
+  public void setNoregistrasi(String noregistrasi) {
+    this.noregistrasi = noregistrasi;
+  }
+  public String getNoregistrasi() {
+    return noregistrasi;
+  }
+  public void setNama(String nama){
+    this.nama = nama;
+  }
+  public String getNama () {
+    return nama;
+  }
+  public void setPeringkat( String peringkat) {
+    this.peringkat = peringkat;
+  }
+  public String getPeringkat () {
+    return peringkat;
+  }
+}
